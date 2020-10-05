4896c04e13106ff2674173f314223db37358cd18
Tempest Campbell <TempestCampbell@hotmail.com>
2020-10-04 19:57:54 -0600

    Completed PyPoll, and Analysis

--- Pypoll/pypollRD.py
+++ Pypoll/pypollRD.py
@@ -4,13 +4,7 @@ import csv
 #Path to collect data from the Resources file
 election_csv = os.path.join('Resources', 'election_data.csv')
 
-#Define the function
-#def votes(election_data):
-
 #Variables
-#voter_ID = int(election_data[0])
-#voter_county = str(election_data[1]
-#candidate_name = str(election_data[2])
 total_votes_cast = 0
 candidates={}
 candidate_votes={}
@@ -30,8 +24,6 @@ with open(election_csv, 'r') as csvfile:
         total_votes_cast += 1
         i = (row[2])
 
-        
-
 ##A complete list of candidates who received votes
 ###The percentage of votes each candidate won
 ###The total number of votes each candidate won
@@ -42,25 +34,11 @@ with open(election_csv, 'r') as csvfile:
         else:
             candidate_votes[i] = 1
 
-    #For loop to calculate percentage
-    for key, votes in candidate_votes.items():
-        percentage_votes = round(((votes/total_votes_cast)*100),3)
-        
-
-##The winner of the election based on popular vote.
-#Conditional to find winner
-        if votes > winner_popularvote:
-            winner_popularvote = votes
-            total_votes_won = key
-
-
     print("Election Results")
     print("-------------------------------")
     print(f'Total Votes:{total_votes_cast}')
     print("-------------------------------")
-    print(f'{key}:{percentage_votes}% ({votes})')
-    print("-------------------------------")
-
+    
 ##In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
 Analysis_file="election_data_results.txt"
@@ -70,8 +48,25 @@ with open(csvpath,'w') as text:
     text.write('Election Results')
     text.write('\n -------------------------------')
     text.write(f' \n Total Votes: {total_votes_cast}')
-    text.write(" \n --------------------------------")
-    text.write(f'\n {key}:{percentage_votes}% ({votes})')
+
+    #For loop to calculate percentage
+    for key, votes in candidate_votes.items():
+        percentage_votes = round(((votes/total_votes_cast)*100),3)
+        print(f'{key}:{percentage_votes}% ({votes})')
+        print("-------------------------------")
+        text.write(" \n --------------------------------")
+        text.write(f'\n {key}:{percentage_votes}% ({votes})')
+        
+##The winner of the election based on popular vote.
+#Conditional to find winner
+        if votes > winner_popularvote:
+            winner_popularvote = votes
+            total_votes_won = key
+
+    print(f"Winner: {total_votes_won}")
+    print("--------------------------------")
     text.write("\n --------------------------------")
-    text.write
+    text.write(f"\n Winner: {total_votes_won}")
+    text.write("\n --------------------------------")
+    
 
